d10f57eb26824fab8ab0dc99d24f7eda47087bd2
Klaus Thelen <klaus.super@gmail.com>
2021-05-19 22:09:57 -0500

    .env used

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import { morganMiddleware } from "./middleware/morgan.js";
 import router from "./router.js";
 import cors from "cors";
 
+import dotenv  from "dotenv";
+dotenv.config()
+
 const app = express();
 app.use(morganMiddleware);
 app.use(express.json());
@@ -10,7 +13,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(router);
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
